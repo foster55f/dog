b673bc63f6393047e865e710f38363ab3b59eb7c
“Foster <fosterataylor@yahoo.com>
2019-06-30 16:47:35 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+var topInput = document.querySelector('.top-input');
+var topButton = document.querySelector('.top-button');
+var someDogs = document.querySelector('.some-dogs');
+
+
+
+topButton.addEventListener("click", changeName);
+
+function changeName(){
+  event.preventDefault();
+  someDogs.innerHTML = topInput.value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//add a document.queryselector
+//add an event listener
+//add a function that manipulates the dom(change dog name)
+
+
+
+
+// function nameDog(){
+  // var nameDogButton = document.querySelector('.top-button');
+  // var nameDogInput = document.querySelector('.top-input');
+  // var newName = nameDogInput.value
+// }
+
+
+// `````add event listener for click on nameDogButton
+// add doc query sel for span of 'some dogs'
+// innerhtml for switching input for the new span
+
+
+
+//this is in js3
+//use this gregory anderson
